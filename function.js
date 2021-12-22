105624f545901c0641bdea8e0f8e5d9057afa3c8
kmsbio <75796960+kmsbio@users.noreply.github.com>
2021-12-23 02:47:40 +0900

    프로그램 시작
    핵심적인 소스코드입니다.

---
+++ function.js
@@ -0,0 +1,64 @@
+var count = 0;
+
+function playGame() {
+    var Name;
+    var Repeat;
+
+    //기본 선물 환경에서 사람뽑기 화면으로 바뀐다.
+    if (count == 0) {
+        document.getElementById("startIMG").remove();
+        document.getElementById("board").innerHTML = "<span id=" + "winning" + "></span>";
+        document.getElementById("log").innerText = "";
+        count++;
+    }
+
+    Name = initName();
+    Game(Name);
+    //textarea에서 입력후 enter 단위로 나누는 함수
+    function initName() {
+        var text = document.getElementById("inputName").value;
+        var splitText = text.split("\n");
+        return splitText;
+    }
+
+    //실질적으로 Game을 주도하는 함수
+    function Game(Name) {
+        var count, tempNum;
+        count = Name.length;
+
+        while (1) {
+            tempNum = Math.floor(Math.random() * count);
+            if (checkRepeat(Name[tempNum], count)) {
+                document.getElementById("winning").innerText = Name[tempNum];
+                document.getElementById("log").innerText += Name[tempNum] + ",";
+                break;
+            }
+        }
+    }
+
+    //선물은 한사람이 여러분 줄 수 없다 log 값을 인식하여 중복을 피한다.
+    function checkRepeat(Name, count) {
+        var text = $("#log").text();
+        var splitText = text.split(",");
+        if (count < splitText.length) {
+            //한번 반복을 돌리면 다음부터는 자유롭게 돌린다.
+            return 1;
+        }
+        for (var i = 0; i < splitText.length; i++) {
+            if (splitText[i] == Name) return 0; //같으면 0을 다시한번 돌린다.
+        }
+        return 1;
+    }
+}
+
+function before() {
+    //before버튼을 누르는 경우 마지막 log를 지운다.
+    var text = $("#log").text();
+    var splitText = text.split(",");
+
+    document.getElementById("log").innerText = "";
+
+    for (var i = 0; i < splitText.length - 2; i++) {
+        document.getElementById("log").innerText += splitText[i] + ",";
+    }
+}
